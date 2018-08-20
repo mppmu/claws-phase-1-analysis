16f0d8be2d29122aa54ae767f4c599ef973dcde8
M.Gabriel <mgabriel@mpp.mpg.de>
2018-08-20 10:23:00 +0200

    Final set of phase 1 parameters

--- CorrectionFactors/extract_correction_factor.C
+++ CorrectionFactors/extract_correction_factor.C
@@ -55,71 +55,89 @@ Double_t langaufun(Double_t *x, Double_t *par) {
 
 void extract_correction_factor()
 {
-    TFile* rfile = new TFile("./run_900122_systematics_ph1v4.root");
+    TFile* rfile = new TFile("./run_900122_systematics_ph1v4_19.7.root");
 
     TH1F* fwd1 = (TH1F*) rfile->Get("FWD1_mip_per_event");
     TH1F* fwd2 = (TH1F*) rfile->Get("FWD2_mip_per_event");
     TH1F* fwd3 = (TH1F*) rfile->Get("FWD3_mip_per_event");
     TH1F* fwd4 = (TH1F*) rfile->Get("FWD4_mip_per_event");
 
-    Double_t pllo[4], plhi[4];
+    TH1F* tres = (TH1F*) rfile->Get("time_resolution");
+    tres->GetXaxis()->SetRangeUser(-20e-9,20e-9);
 
-    pllo[0]=0.0; pllo[1]=0.0;   pllo[2]=0; pllo[3]=0;
-    plhi[0]=1.0; plhi[1]=2.0; plhi[2]=1000000.0; plhi[3]=2.0;
+    TF1 *langaus1 = fwd1->GetFunction("FWD1_mip_per_eventlangaus");
+    TF1 *langaus2 = fwd2->GetFunction("FWD2_mip_per_eventlangaus");
+    TF1 *langaus3 = fwd3->GetFunction("FWD3_mip_per_eventlangaus");
+    TF1 *langaus4 = fwd4->GetFunction("FWD4_mip_per_eventlangaus");
 
-    double rlow1 = fwd1->GetBinLowEdge(5);
-    double rup1 = fwd1->GetMean()*4.0;
+    // Double_t pllo[4], plhi[4];
+    //
+    // pllo[0]=0.0; pllo[1]=0.0;   pllo[2]=0; pllo[3]=0;
+    // plhi[0]=1.0; plhi[1]=2.0; plhi[2]=1000000.0; plhi[3]=2.0;
+    //
+    // double rlow1 = fwd1->GetBinLowEdge(5);
+    // double rup1 = fwd1->GetMean()*4.0;
+    //
+    // double rlow2 = fwd2->GetBinLowEdge(5);
+    // double rup2 = fwd2->GetMean()*4.0;
+    //
+    // double rlow3 = fwd3->GetBinLowEdge(5);
+    // double rup3 = fwd3->GetMean()*4.0;
+    //
+    // double rlow4 = fwd4->GetBinLowEdge(5);
+    // double rup4 = fwd4->GetMean()*4.0;
+    // // Shit basically stolen from the root example:
+    // // https://root.cern.ch/root/html/tutorials/fit/langaus.C.html
+    // TF1 *langaus1 = new TF1("langaus",langaufun, rlow1, rup1,4);
+    // TF1 *langaus2 = new TF1("langaus",langaufun, rlow2, rup2,4);
+    // TF1 *langaus3 = new TF1("langaus",langaufun, rlow3, rup3,4);
+    // TF1 *langaus4 = new TF1("langaus",langaufun, rlow4, rup4,4);
+    //
+    // double par1 = fwd1->GetBinCenter(fwd1->GetMaximumBin());
+    // double par2 = fwd1->GetBinCenter(fwd2->GetMaximumBin());
+    // double par3 = fwd1->GetBinCenter(fwd3->GetMaximumBin());
+    // double par4 = fwd1->GetBinCenter(fwd4->GetMaximumBin());
+    //
+    // langaus1->SetParameters(0.1, par1, 500., 0.1);
+    // langaus1->SetParNames("Width","MP","Area","GSigma");
+    //
+    // langaus2->SetParameters(0.1, par2, 500., 0.1);
+    // langaus2->SetParNames("Width","MP","Area","GSigma");
+    //
+    // langaus3->SetParameters(0.1, par3, 500., 0.1);
+    // langaus3->SetParNames("Width","MP","Area","GSigma");
+    //
+    // langaus4->SetParameters(0.1, par4, 500., 0.1);
+    // langaus4->SetParNames("Width","MP","Area","GSigma");
+    //
+    // for ( int j = 0; j < 4; ++j)
+    // {
+    //         langaus1->SetParLimits(j, pllo[j], plhi[j]);
+    //         langaus2->SetParLimits(j, pllo[j], plhi[j]);
+    //         langaus3->SetParLimits(j, pllo[j], plhi[j]);
+    //         langaus4->SetParLimits(j, pllo[j], plhi[j]);
+    // }
+    //
+    //
+    // fwd1->Fit(langaus1, "QRSL");
+    // fwd2->Fit(langaus2, "QRSL");
+    // fwd3->Fit(langaus3, "QRSL");
+    // fwd4->Fit(langaus4, "QRSL");
 
-    double rlow2 = fwd2->GetBinLowEdge(5);
-    double rup2 = fwd2->GetMean()*4.0;
+    cout << "Max 1: " << langaus1->GetMaximumX() << endl;
+    cout << "Max 2: " << langaus2->GetMaximumX() << endl;
+    cout << "Max 3: " << langaus3->GetMaximumX() << endl;
+    cout << "Max 4: " << langaus4->GetMaximumX() << endl;
 
-    double rlow3 = fwd3->GetBinLowEdge(5);
-    double rup3 = fwd3->GetMean()*4.0;
+    cout << "Corr 1: " << 1-langaus1->GetMaximumX() << endl;
+    cout << "Corr 2: " << 1-langaus2->GetMaximumX() << endl;
+    cout << "Corr 3: " << 1-langaus3->GetMaximumX() << endl;
+    cout << "Corr 4: " << 1-langaus4->GetMaximumX() << endl;
 
-    double rlow4 = fwd4->GetBinLowEdge(5);
-    double rup4 = fwd4->GetMean()*4.0;
-    // Shit basically stolen from the root example:
-    // https://root.cern.ch/root/html/tutorials/fit/langaus.C.html
-    TF1 *langaus1 = new TF1("langaus",langaufun, rlow1, rup1,4);
-    TF1 *langaus2 = new TF1("langaus",langaufun, rlow2, rup2,4);
-    TF1 *langaus3 = new TF1("langaus",langaufun, rlow3, rup3,4);
-    TF1 *langaus4 = new TF1("langaus",langaufun, rlow4, rup4,4);
+    TF1* gaus = tres->GetFunction("gaus");
 
-    double par1 = fwd1->GetBinCenter(fwd1->GetMaximumBin());
-    double par2 = fwd1->GetBinCenter(fwd2->GetMaximumBin());
-    double par3 = fwd1->GetBinCenter(fwd3->GetMaximumBin());
-    double par4 = fwd1->GetBinCenter(fwd4->GetMaximumBin());
-
-    langaus1->SetParameters(0.1, par1, 500., 0.1);
-    langaus1->SetParNames("Width","MP","Area","GSigma");
-
-    langaus2->SetParameters(0.1, par2, 500., 0.1);
-    langaus2->SetParNames("Width","MP","Area","GSigma");
-
-    langaus3->SetParameters(0.1, par3, 500., 0.1);
-    langaus3->SetParNames("Width","MP","Area","GSigma");
-
-    langaus4->SetParameters(0.1, par4, 500., 0.1);
-    langaus4->SetParNames("Width","MP","Area","GSigma");
-
-    for ( int j = 0; j < 4; ++j)
-    {
-            langaus1->SetParLimits(j, pllo[j], plhi[j]);
-            langaus2->SetParLimits(j, pllo[j], plhi[j]);
-            langaus3->SetParLimits(j, pllo[j], plhi[j]);
-            langaus4->SetParLimits(j, pllo[j], plhi[j]);
-    }
-
-
-    fwd1->Fit(langaus1, "QRSL");
-    fwd2->Fit(langaus2, "QRSL");
-    fwd3->Fit(langaus3, "QRSL");
-    fwd4->Fit(langaus4, "QRSL");
-
-    cout << "Max 1: " << 1-langaus1->GetMaximumX() << endl;
-    cout << "Max 2: " << 1-langaus2->GetMaximumX() << endl;
-    cout << "Max 3: " << 1-langaus3->GetMaximumX() << endl;
-    cout << "Max 4: " << 1-langaus4->GetMaximumX() << endl;
+    cout << "TRes: " << gaus->GetParameter(2) << endl;
+    cout << "TRes shift: " << gaus->GetParameter(1) << endl;
 
     double mean = 1-langaus1->GetMaximumX() + 1-langaus2->GetMaximumX() + 1-langaus3->GetMaximumX() + 1-langaus4->GetMaximumX();
     mean /= 4.;
@@ -135,33 +153,42 @@ void extract_correction_factor()
     can->SetBorderSize(2);
     can->SetFrameBorderMode(0);
 
+    can->Divide(3,2);
 //    double
-    TPad* pad1 = new TPad("pad1", "The pad 80% of the height", 0., 0.5, 0.5, 1);
+    //TPad* pad1 = new TPad("pad1", "The pad 80% of the height", 0., 0.5, 0.5, 1);
 //    pad1->SetLeftMargin( leftmargin );
 //    pad1->SetBottomMargin( 0.13 );
     //pad1->SetLeftMargin( leftmargin );
 
-    pad1->Draw();
+//    pad1->Draw();
 
-    TPad* pad2 = new TPad("pad2", "The pad 80% of the height", 0.5, 0.5, 1, 1);
-    pad2->Draw();
+    // TPad* pad2 = new TPad("pad2", "The pad 80% of the height", 0.5, 0.5, 1, 1);
+    // pad2->Draw();
+    //
+    // TPad* pad3 = new TPad("pad3", "The pad 80% of the height", 0., 0., 0.5, 0.5);
+    // pad3->Draw();
+    //
+    // TPad* pad4 = new TPad("pad4", "The pad 80% of the height", 0.5, 0, 1, 0.5);
+    // pad4->Draw();
 
-    TPad* pad3 = new TPad("pad3", "The pad 80% of the height", 0., 0., 0.5, 0.5);
-    pad3->Draw();
-
-    TPad* pad4 = new TPad("pad4", "The pad 80% of the height", 0.5, 0, 1, 0.5);
-    pad4->Draw();
-
-    pad1->cd();
+//    pad1->cd();
+    can->cd(1);
     fwd1->Draw();
 
-    pad2->cd();
+//    pad2->cd();
+    can->cd(2);
     fwd2->Draw();
 
-    pad3->cd();
+    TPad* pad = (TPad*) can->cd(3);
+    pad->SetLogy();
+    tres->Draw();
+
+    can->cd(4);
+//    pad3->cd();
     fwd3->Draw();
 
-    pad4->cd();
+    can->cd(5);
+//    pad4->cd();
     fwd4->Draw();
 
 }
--- Scripts/plot_waveform.cxx
+++ Scripts/plot_waveform.cxx
@@ -32,227 +32,227 @@ using namespace std;
 
 void plot_waveform(){
 
-    // #ifdef __CINT__
-    //     gROOT->LoadMacro("Belle2Style.C");
-    //     gROOT->LoadMacro("Belle2Utils.C");
-    //     gROOT->LoadMacro("Belle2Labels.C");
-    // #endif
-        // gROOT->LoadMacro("Belle2Style.C");
-        SetBelle2Style();
-        gStyle->SetOptStat(0);
+		// #ifdef __CINT__
+		//     gROOT->LoadMacro("Belle2Style.C");
+		//     gROOT->LoadMacro("Belle2Utils.C");
+		//     gROOT->LoadMacro("Belle2Labels.C");
+		// #endif
+		// gROOT->LoadMacro("Belle2Style.C");
+		SetBelle2Style();
+		gStyle->SetOptStat(0);
 
-        // Int_t ci = TColor::GetFreeColorIndex();
-        TColor *red = new TColor(0.84, 0.15, 0.16);
-        TColor *red2 = new TColor(213./255, 61./255, 53./255);
-        TColor *orange = new TColor(255./255, 127./255, 14./255 );
-        TColor *blue = new TColor(0.12, 0.47, 0.71);
-        TColor *lblue = new TColor(0.68, 0.78, 0.91);
-        TColor *green = new TColor(0.17, 0.63, 0.17);
-        TColor *gray = new TColor(165./255, 172./255,175./255 );
-        TColor *antra = new TColor(34./255, 40./255,46./255 );
+		// Int_t ci = TColor::GetFreeColorIndex();
+		TColor *red = new TColor(0.84, 0.15, 0.16);
+		TColor *red2 = new TColor(213./255, 61./255, 53./255);
+		TColor *orange = new TColor(255./255, 127./255, 14./255 );
+		TColor *blue = new TColor(0.12, 0.47, 0.71);
+		TColor *lblue = new TColor(0.68, 0.78, 0.91);
+		TColor *green = new TColor(0.17, 0.63, 0.17);
+		TColor *gray = new TColor(165./255, 172./255,175./255 );
+		TColor *antra = new TColor(34./255, 40./255,46./255 );
 
-        int rebin = 100;
-        double binsize = 0.8;
-        double timescaling = 0.001;
-        int triggershift = 134310;
-        // -------------------------------
-        // Get the histograms from file
-        // -------------------------------
+		int rebin = 100;
+		double binsize = 0.8;
+		double timescaling = 0.001;
+		int triggershift = 134310;
+		// -------------------------------
+		// Get the histograms from file
+		// -------------------------------
 
-        //    std::string fname = std::string(argv[1]) + "/" + std::string(argv[2]);
-        // std::string fname = "/remote/ceph/group/ilc/claws/data/Analysis/Run_401131/run_401131";
-        std::string fname = "/remote/ceph/group/ilc/claws/data/Analysis/Run_401141/run_401141";
+		//    std::string fname = std::string(argv[1]) + "/" + std::string(argv[2]);
+		// std::string fname = "/remote/ceph/group/ilc/claws/data/Analysis/Run_401131/run_401131";
+		std::string fname = "/remote/ceph/group/ilc/claws/data/Analysis/Run_401141/run_401141";
 
-        TFile* file = new TFile( (fname+".root").c_str() , "open");
+		TFile* file = new TFile( (fname+".root").c_str(), "open");
 
-        if( file->IsZombie() )
-        {
-            cout << "Error openning file" << endl;
-            exit(-1);
-        }
-
-
-
-        TH1F * fwd1 = (TH1F*) file->Get( "FWD1" );
-        fwd1->SetBins(fwd1->GetNbinsX(),(0.5- triggershift)*binsize*timescaling , (fwd1->GetNbinsX()+0.5 - triggershift )*binsize*timescaling );
-        // std::cout << "First: " << fwd1->Integral() << std::endl;
-        fwd1->Rebin(rebin);
-
-        //     if(fwd1->GetBinContent(i) <= noplot) fwd1->SetBinContent(i, 0);
-        // }
-        // std::cout << "Second: " << fwd1->Integral() << std::endl;
-        TH1F * fwd2 = (TH1F*) file->Get( "FWD2" );
-        fwd2->SetBins(fwd2->GetNbinsX(),(0.5- triggershift)*binsize*timescaling, (fwd2->GetNbinsX()+0.5 - triggershift)*binsize*timescaling );
-        fwd2->Rebin(rebin);
-        // for(int i = 1 ; i< fwd2->GetNbinsX(); i++)
-        // {
-        //     if(fwd2->GetBinContent(i) <= noplot) fwd2->SetBinContent(i, 0);
-        // }
-        TH1F * fwd3 = (TH1F*) file->Get( "FWD3" );
-        fwd3->SetBins(fwd3->GetNbinsX(), (0.5 - triggershift)*binsize*timescaling, (fwd3->GetNbinsX()+0.5 - triggershift)*binsize*timescaling );
-        fwd3->Rebin(rebin);
-
-        // for(int i = 1 ; i< fwd3->GetNbinsX(); i++)
-        // {
-        //     if(fwd3->GetBinContent(i) <= noplot) fwd3->SetBinContent(i, 0);
-        // }
-
-        // ---------------------------------------
-        // make the hists nice
-        // ---------------------------------------
-
-        int linewidth = 3;
-        fwd1->SetFillColor(green->GetNumber());
-    //    fwd1->SetLineColorAlpha(green->GetNumber(), 0.2);
-        fwd1->SetLineColor(red->GetNumber());
-        fwd1->SetLineWidth(linewidth);
-
-        fwd2->SetFillColor(red->GetNumber());
-        // fwd2->SetMarkerColor(kViolet-3);
-        //fwd2->SetLineColorAlpha(red->GetNumber(), 0.2);
-        fwd2->SetLineColor(gray->GetNumber());
-        fwd2->SetLineWidth(linewidth);
-        // fwd2->SetMarkerStyle(21);
-        // fwd2->SetMarkerSize(1.5);
-
-        fwd3->SetFillColor(blue->GetNumber());
-        // hist->SetFillStyle(3002);
-        // fwd3->SetLineColorAlpha(blue->GetNumber(), 0.5);
-    //    fwd3->SetLineColor(antra->GetNumber());
-        fwd3->SetLineColor(antra->GetNumber());
-        fwd3->SetLineWidth(linewidth);
-
-
-        double markersize = 0.6;
-        // double
-        fwd1->SetMarkerStyle(20);
-        fwd1->SetMarkerSize(markersize);
-
-        fwd2->SetMarkerStyle(20);
-        fwd2->SetMarkerSize(markersize);
-
-        fwd3->SetMarkerStyle(20);
-        fwd3->SetMarkerSize(markersize);
+		if( file->IsZombie() )
+		{
+				cout << "Error openning file" << endl;
+				exit(-1);
+		}
 
 
 
-        // -----------------------------------
-        // Make the canvas and plot the stack
-        // -----------------------------------
+		TH1F * fwd1 = (TH1F*) file->Get( "FWD1" );
+		fwd1->SetBins(fwd1->GetNbinsX(),(0.5- triggershift)*binsize*timescaling, (fwd1->GetNbinsX()+0.5 - triggershift )*binsize*timescaling );
+		// std::cout << "First: " << fwd1->Integral() << std::endl;
+		fwd1->Rebin(rebin);
 
-        TCanvas* c1 = new TCanvas("c1","inclusive production of pions at Y(4S)",50,50,600,600);
-        TPad* thePad = (TPad*)c1->cd();
+		//     if(fwd1->GetBinContent(i) <= noplot) fwd1->SetBinContent(i, 0);
+		// }
+		// std::cout << "Second: " << fwd1->Integral() << std::endl;
+		TH1F * fwd2 = (TH1F*) file->Get( "FWD2" );
+		fwd2->SetBins(fwd2->GetNbinsX(),(0.5- triggershift)*binsize*timescaling, (fwd2->GetNbinsX()+0.5 - triggershift)*binsize*timescaling );
+		fwd2->Rebin(rebin);
+		// for(int i = 1 ; i< fwd2->GetNbinsX(); i++)
+		// {
+		//     if(fwd2->GetBinContent(i) <= noplot) fwd2->SetBinContent(i, 0);
+		// }
+		TH1F * fwd3 = (TH1F*) file->Get( "FWD3" );
+		fwd3->SetBins(fwd3->GetNbinsX(), (0.5 - triggershift)*binsize*timescaling, (fwd3->GetNbinsX()+0.5 - triggershift)*binsize*timescaling );
+		fwd3->Rebin(rebin);
 
-        THStack *stack = new THStack("stack","");
-        stack->Add(fwd1);
-        stack->Add(fwd2);
-        stack->Add(fwd3);
+		// for(int i = 1 ; i< fwd3->GetNbinsX(); i++)
+		// {
+		//     if(fwd3->GetBinContent(i) <= noplot) fwd3->SetBinContent(i, 0);
+		// }
 
-        // // stack->GetXaxis()->SetRangeUser(0,1000);
-        // stack->SetMinimum(0);
-        // stack->SetMaximum(250);
+		// ---------------------------------------
+		// make the hists nice
+		// ---------------------------------------
 
-        stack->Draw();
+		int linewidth = 3;
+		fwd1->SetFillColor(green->GetNumber());
+		//    fwd1->SetLineColorAlpha(green->GetNumber(), 0.2);
+		fwd1->SetLineColor(red->GetNumber());
+		fwd1->SetLineWidth(linewidth);
 
-        stack->GetYaxis()->SetTitle("Charged particle rate [MIP/80ns]");
-        stack->GetXaxis()->SetTitle("Time [#mus]");
+		fwd2->SetFillColor(red->GetNumber());
+		// fwd2->SetMarkerColor(kViolet-3);
+		//fwd2->SetLineColorAlpha(red->GetNumber(), 0.2);
+		fwd2->SetLineColor(gray->GetNumber());
+		fwd2->SetLineWidth(linewidth);
+		// fwd2->SetMarkerStyle(21);
+		// fwd2->SetMarkerSize(1.5);
 
-        stack->GetXaxis()->SetRangeUser(-50,2500);
-
-        // For runs 401141 and 401140 [0,14]
-        // For runs 401145 and 401131 [0,250]
-
-        stack->SetMinimum(0);
-        stack->SetMaximum(250);
-
-        c1->Modified();
-
-
-    double boxsize = 0.04;
-    double height = 0.885;
-     myBoxText(    0.465, height, boxsize, red->GetNumber(), "FWD1");
-     myBoxText(    0.635, height, boxsize, gray->GetNumber(), "FWD2");
-     myBoxText(    0.805, height, boxsize, antra->GetNumber(), "FWD3");
-
-
-    double scalingfactor = 1/(0.94-0.4)*0.75;
-
-    TPad * pad2 = new TPad("pad2", "The pad 20% of the height", 0.4, 0.3, 0.94, 0.84, kWhite);
-
-    pad2->Draw();
-    pad2->cd();
-    pad2->SetRightMargin(0.06);
-    pad2->SetTopMargin(0.01);
-
-    THStack* stack_2 = (THStack*) stack->Clone();
-
-    // For runs 401141  [-5,145] HER Ref
-    // For runs 401131  [115,315] LER Phase
-    // For runs 401140 [-5,145]   HER Phase
-    // For runs 401145 [115,315]  LER Ref
-    stack_2->GetXaxis()->SetRangeUser(115,315);
-    stack_2->GetXaxis()->SetNdivisions(505);
-
-    // For runs 401145 and 401131 [0,35]
-    // For runs 401141 and 401140 [0,2.6]
-    stack_2->SetMinimum(0);
-
-    stack_2->SetMaximum(35);
-
-    stack_2->Draw();
-
-    stack_2->GetXaxis()->SetTitleSize(stack_2->GetXaxis()->GetTitleSize()*scalingfactor);
-    stack_2->GetYaxis()->SetTitleSize(stack_2->GetYaxis()->GetTitleSize()*scalingfactor);
-
-    stack_2->GetXaxis()->SetTitleOffset(stack_2->GetXaxis()->GetTitleOffset()/scalingfactor);
-    stack_2->GetYaxis()->SetTitleOffset(stack_2->GetYaxis()->GetTitleOffset()*1.12/scalingfactor);
-
-    stack_2->GetXaxis()->SetLabelSize(stack_2->GetXaxis()->GetLabelSize()*scalingfactor);
-    stack_2->GetYaxis()->SetLabelSize(stack_2->GetYaxis()->GetLabelSize()*scalingfactor);
+		fwd3->SetFillColor(blue->GetNumber());
+		// hist->SetFillStyle(3002);
+		// fwd3->SetLineColorAlpha(blue->GetNumber(), 0.5);
+		//    fwd3->SetLineColor(antra->GetNumber());
+		fwd3->SetLineColor(antra->GetNumber());
+		fwd3->SetLineWidth(linewidth);
 
 
-    pad2->Modified();
+		double markersize = 0.6;
+		// double
+		fwd1->SetMarkerStyle(20);
+		fwd1->SetMarkerSize(markersize);
 
+		fwd2->SetMarkerStyle(20);
+		fwd2->SetMarkerSize(markersize);
 
-    // double boxsize = 0.04;
-    //
-    //  myBoxText(    0.44, 0.25, boxsize, green->GetNumber(), "FWD1");
-    //  myBoxText(    0.63, 0.25, boxsize, red->GetNumber(), "FWD2");
-    //  myBoxText(    0.82, 0.25, boxsize, blue->GetNumber(), "FWD3");
-
-    //  myBoxText(    0.7, 0.74, boxsize, green->GetNumber(), "FWD1");
-    //  myBoxText(    0.7, 0.69, boxsize, red->GetNumber(), "FWD2");
-    //  myBoxText(    0.7, 0.64, boxsize, blue->GetNumber(), "FWD3");
+		fwd3->SetMarkerStyle(20);
+		fwd3->SetMarkerSize(markersize);
 
 
 
-    std::vector<std::string> formats = {".jpg",".eps",".png",".pdf"};
+		// -----------------------------------
+		// Make the canvas and plot the stack
+		// -----------------------------------
 
-    for(auto& format : formats)
-    {
-    //    c1->SaveAs( (fname + format).c_str() );
-        c1->SaveAs( ("InjectionMIPFinder_FWD_401141_new" + format).c_str() );
-    }
+		TCanvas* c1 = new TCanvas("c1","inclusive production of pions at Y(4S)",50,50,600,600);
+		TPad* thePad = (TPad*)c1->cd();
 
-    // -----------------------------------
-    // Get the integrals:
-    // -----------------------------------
+		THStack *stack = new THStack("stack","");
+		stack->Add(fwd1);
+		stack->Add(fwd2);
+		stack->Add(fwd3);
 
-    ofstream myfile;
-    myfile.open("401141_integrals.txt");
+		// // stack->GetXaxis()->SetRangeUser(0,1000);
+		// stack->SetMinimum(0);
+		// stack->SetMaximum(250);
 
-    int limit = 1000;
-    myfile << "Time[us]: " << limit << std::endl;
-    myfile << "Percentage FWD1: " << fwd1->Integral(1,(int)(limit/0.080))/fwd1->Integral() << std::endl;
-    myfile << "Percentage FWD2: " << fwd2->Integral(1,(int)(limit/0.080))/fwd2->Integral() << std::endl;
-    myfile << "Percentage FWD3: " << fwd3->Integral(1,(int)(limit/0.080))/fwd3->Integral() << std::endl;
+		stack->Draw();
 
-    limit = 1500;
-    myfile << "Time[us]: " << limit << std::endl;
-    myfile << "Percentage FWD1: " << fwd1->Integral(1,(int)(limit/0.080))/fwd1->Integral() << std::endl;
-    myfile << "Percentage FWD2: " << fwd2->Integral(1,(int)(limit/0.080))/fwd2->Integral() << std::endl;
-    myfile << "Percentage FWD3: " << fwd3->Integral(1,(int)(limit/0.080))/fwd3->Integral() << std::endl;
+		stack->GetYaxis()->SetTitle("Charged particle rate [MIP/80ns]");
+		stack->GetXaxis()->SetTitle("Time [#mus]");
 
-    myfile.close();
+		stack->GetXaxis()->SetRangeUser(-50,2500);
+
+		// For runs 401141 and 401140 [0,14]
+		// For runs 401145 and 401131 [0,250]
+
+		stack->SetMinimum(0);
+		stack->SetMaximum(250);
+
+		c1->Modified();
+
+
+		double boxsize = 0.04;
+		double height = 0.885;
+		myBoxText(    0.465, height, boxsize, red->GetNumber(), "FWD1");
+		myBoxText(    0.635, height, boxsize, gray->GetNumber(), "FWD2");
+		myBoxText(    0.805, height, boxsize, antra->GetNumber(), "FWD3");
+
+
+		double scalingfactor = 1/(0.94-0.4)*0.75;
+
+		TPad * pad2 = new TPad("pad2", "The pad 20% of the height", 0.4, 0.3, 0.94, 0.84, kWhite);
+
+		pad2->Draw();
+		pad2->cd();
+		pad2->SetRightMargin(0.06);
+		pad2->SetTopMargin(0.01);
+
+		THStack* stack_2 = (THStack*) stack->Clone();
+
+		// For runs 401141  [-5,145] HER Ref
+		// For runs 401131  [115,315] LER Phase
+		// For runs 401140 [-5,145]   HER Phase
+		// For runs 401145 [115,315]  LER Ref
+		stack_2->GetXaxis()->SetRangeUser(115,315);
+		stack_2->GetXaxis()->SetNdivisions(505);
+
+		// For runs 401145 and 401131 [0,35]
+		// For runs 401141 and 401140 [0,2.6]
+		stack_2->SetMinimum(0);
+
+		stack_2->SetMaximum(35);
+
+		stack_2->Draw();
+
+		stack_2->GetXaxis()->SetTitleSize(stack_2->GetXaxis()->GetTitleSize()*scalingfactor);
+		stack_2->GetYaxis()->SetTitleSize(stack_2->GetYaxis()->GetTitleSize()*scalingfactor);
+
+		stack_2->GetXaxis()->SetTitleOffset(stack_2->GetXaxis()->GetTitleOffset()/scalingfactor);
+		stack_2->GetYaxis()->SetTitleOffset(stack_2->GetYaxis()->GetTitleOffset()*1.12/scalingfactor);
+
+		stack_2->GetXaxis()->SetLabelSize(stack_2->GetXaxis()->GetLabelSize()*scalingfactor);
+		stack_2->GetYaxis()->SetLabelSize(stack_2->GetYaxis()->GetLabelSize()*scalingfactor);
+
+
+		pad2->Modified();
+
+
+		// double boxsize = 0.04;
+		//
+		//  myBoxText(    0.44, 0.25, boxsize, green->GetNumber(), "FWD1");
+		//  myBoxText(    0.63, 0.25, boxsize, red->GetNumber(), "FWD2");
+		//  myBoxText(    0.82, 0.25, boxsize, blue->GetNumber(), "FWD3");
+
+		//  myBoxText(    0.7, 0.74, boxsize, green->GetNumber(), "FWD1");
+		//  myBoxText(    0.7, 0.69, boxsize, red->GetNumber(), "FWD2");
+		//  myBoxText(    0.7, 0.64, boxsize, blue->GetNumber(), "FWD3");
+
+
+
+		std::vector<std::string> formats = {".jpg",".eps",".png",".pdf"};
+
+		for(auto& format : formats)
+		{
+				//    c1->SaveAs( (fname + format).c_str() );
+				c1->SaveAs( ("InjectionMIPFinder_FWD_401141_new" + format).c_str() );
+		}
+
+		// -----------------------------------
+		// Get the integrals:
+		// -----------------------------------
+
+		ofstream myfile;
+		myfile.open("401141_integrals.txt");
+
+		int limit = 1000;
+		myfile << "Time[us]: " << limit << std::endl;
+		myfile << "Percentage FWD1: " << fwd1->Integral(1,(int)(limit/0.080))/fwd1->Integral() << std::endl;
+		myfile << "Percentage FWD2: " << fwd2->Integral(1,(int)(limit/0.080))/fwd2->Integral() << std::endl;
+		myfile << "Percentage FWD3: " << fwd3->Integral(1,(int)(limit/0.080))/fwd3->Integral() << std::endl;
+
+		limit = 1500;
+		myfile << "Time[us]: " << limit << std::endl;
+		myfile << "Percentage FWD1: " << fwd1->Integral(1,(int)(limit/0.080))/fwd1->Integral() << std::endl;
+		myfile << "Percentage FWD2: " << fwd2->Integral(1,(int)(limit/0.080))/fwd2->Integral() << std::endl;
+		myfile << "Percentage FWD3: " << fwd3->Integral(1,(int)(limit/0.080))/fwd3->Integral() << std::endl;
+
+		myfile.close();
 
 }
